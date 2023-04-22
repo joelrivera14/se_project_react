3dccbd7a09c33b738a09a30ac1e02978f9faeedf
joel rivera <jorivera114@gmail.com>
2023-04-22 14:17:53 -0400

    10

--- dev/se_project_react/src/Components/App.js
+++ dev/se_project_react/src/Components/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "../logo.svg";
+import "../Blocks/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <div className="header__logo-container">
+          <div className="header__logo">
+            <img src="/images/wtwrlogo.svg" alt="wtwr logo" />
+          </div>
+          <div className="header__date">June 15, New York</div>
+        </div>
+
+        <div className="header__avatar-logo-container">
+          <div className="header__button">
+            <button type="text">+ Add clothes</button>
+          </div>
+          <div className="header__name">name</div>
+          <div className="header__avatar">
+            <img src="/images/avatar.svg" alt="avatar" />
+          </div>
+        </div>
       </header>
+      <main></main>
+      <footer></footer>
+      <div className="modalWithForm"></div>
+      <div className="itemModal"></div>
     </div>
   );
 }
