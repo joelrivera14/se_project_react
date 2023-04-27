1a142dfd168c73b092a19a7fe8d4f88226da98b6
joel rivera <jorivera114@gmail.com>
2023-04-27 06:34:11 -0400

    10

--- src/components/WeatherApi.js
+++ src/components/WeatherApi.js
@@ -1,7 +1,7 @@
-const latitude = 44.34;
-const longitude = 10.99;
-const APIkey = "d4068566f6b20e7786c16bddf5f1ad55";
 export const getForecastWeather = () => {
+  const latitude = 44.34;
+  const longitude = 10.99;
+  const APIkey = "d4068566f6b20e7786c16bddf5f1ad55";
   const weatherApi = fetch(
     ` https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
   ).then((res) => {
