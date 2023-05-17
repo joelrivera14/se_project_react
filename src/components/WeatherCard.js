38e2a4721fc80db975de18f874e1639ddb8b0150
joel rivera <jorivera114@gmail.com>
2023-05-16 21:37:17 -0400

    11

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,6 +1,6 @@
 import "../blocks/Weather.css";
 import "../blocks/card.css";
-import CurrentTempUnitContext from "./CurrentTempUnitContext";
+import CurrentTempUnitContext from "../contexts/CurrentTempUnitContext";
 import React, { useContext } from "react";
 import { temperature } from "../utils/WeatherApi";
 
